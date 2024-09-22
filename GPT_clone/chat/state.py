da938a3ff5a61656990896042b3327a30ad63611
Ritchy <iamrichardcn@gmail.com>
2024-09-22 22:43:34 +0100

    storing chat data 1

--- GPT_clone/chat/state.py
+++ GPT_clone/chat/state.py
@@ -3,6 +3,14 @@ import asyncio
 import reflex as rx
 
 
+
+class ChatMessage(rx.Base):
+    message: str
+    is_bot: bool = False
+    
+    
+    
+
 class ChatState(rx.State):
     did_submit: bool =False
     
@@ -14,9 +22,20 @@ class ChatState(rx.State):
     async def handle_submit(self, form_data:dict):
         # Handle form submission
         print("HERE IS OUR FORM DATA:", form_data)
-        self.did_submit = True
-        yield
-        await asyncio.sleep(2)
-        self.did_submit = False
-        yield
+        user_message=form_data.get('message')
+        if  user_message:
+            self.did_submit = True
+            chat_message= ChatMessage(
+                message=user_message,
+                is_bot=False
+                
+                )
+            yield
+            await asyncio.sleep(2)
+            self.did_submit = False
+            chat_message= ChatMessage(
+                message=user_message,
+                is_bot=True
+            )
+            yield
     
