00f74392b8381f51125f0e0f4ad5811736ee629c
Ritchy <iamrichardcn@gmail.com>
2024-10-06 00:41:26 +0100

    integrate the neon postgress db

--- rxconfig.py
+++ rxconfig.py
@@ -1,5 +1,11 @@
 import reflex as rx
+from decouple import config
+
+
+DATABASE_URL = config("DATABASE_URL", cast=str, default=None)
+
 
 config = rx.Config(
     app_name="GPT_clone",
+    db_url=DATABASE_URL
 )
